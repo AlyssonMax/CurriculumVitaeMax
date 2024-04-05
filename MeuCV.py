043fed990eec3881bb0e8fbfb15d3923963b5985
Alysson Bárbara <63023238+AlyssonMax@users.noreply.github.com>
2024-04-04 22:27:38 -0300

    Add files via upload

---
+++ MeuCV.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Quem é Alysson 'MAX' Bárbara")
+st.write("---")
+
+#cria um container de apresentação
+with st.container():
+    st.subheader("Curriculum Vitae")
+    st.title("Quem é Alysson 'MAX' Bárbara")
+
+#texto de apresentação
+st.write("---")
+st.write("Olá! Me chamo Alysson, um engenheiro de dados entusiasmado e dedicado. Como mineiro que sou, trago para o trabalho a mesma paixão que tenho pela vida familiar. Casado e pai orgulhoso de dois filhos. Desde setembro de 2021, venho me aplicando para não apenas crescer profissionalmente, mas também para contribuir com as equipes próximas a mim. Estou sempre em busca de crescimento na minha área e, atualmente, estou focado em aprimorar minhas habilidades em plataformas líderes de mercado como Databricks, Azure e AWS, além de aprofundar meu conhecimento na linguagem Python. Acredito que a constante evolução técnica e a troca de experiências são fundamentais para o sucesso, e é nisso que baseio minha jornada profissional.")
+
+#cria uma função para carregar os dados fora do container
+#armazena os dados em cache para que não seja necessário recarregar numa segunda execução
+@st.cache_data
+def carrega_dados():
+    tabela_skill = pd.read_csv("SKILLS.csv")
+    return tabela_skill
+st.write("---")
+
+#tabela de cursos
+st.write("CURSOS / CERTIFICADOS / GRADUAÇÕES")
+st.write("---")
+with st.container():
+        df = pd.read_excel("Cursos.xlsx")
+        st.dataframe(df)
+
+st.write("---")
+
+#cria um container para criar um gráfico para habilidades
+st.write("HABILIDADES E PROFICIÊNCIA")
+st.write("---")
+with st.container():
+    dados=carrega_dados()
+    st.bar_chart(dados, x="Nível", y="Habilidade",color="Nível")
+
+st.write("---")
+#lista de soft Skills
+with st.container():
+    st.write("SOFT SKILLS")
+    st.write("---")
+    st.write("Flexibilidade: Adaptabilidade a novos ambientes e desafios.")
+
+    st.write("Resiliência Profissional: Habilidade de lidar com pressão e superar obstáculos.")
+
+    st.write("Aprendizado Contínuo: Compromisso com o desenvolvimento pessoal e profissional.")
+
+    st.write("Atuação junto de agentes multidisciplinares")
+
+    st.write("Resolução de Problemas: Identificar problemas e implementar soluções eficazes1.")
+
+    st.write("Ética de Trabalho: Demonstrar comprometimento, responsabilidade e dedicação ao trabalho")
+
+st.write("---")
+with st.container():
+    st.write("LinkedIn pelo [LINK](https://www.linkedin.com/in/alyssonbarbara/)")
+    
+    st.write("Whatsapp pelo [LINK](https://wa.me/qr/QBC2WTSAJTDBN1)")
+st.write("---")
