20d6b4e5c4b770272bbe6e35afc19a0d2ad7396b
Alysson Bárbara <63023238+AlyssonMax@users.noreply.github.com>
2024-04-07 14:56:43 -0300

    Add files via upload

--- MeuCV.py
+++ MeuCV.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Quem é Alysson 'MAX' Bárbara")
+# Usando st.markdown com HTML para criar um divisor colorido
 st.write("---")
-
 #cria um container de apresentação
 with st.container():
-    st.subheader("Curriculum Vitae")
-    st.title("Quem é Alysson 'MAX' Bárbara")
+    st.subheader("Curriculum Vitae", divider='rainbow')
+    st.title("Quem é _Alysson :blue['MAX'] Bárbara_")
 
 #texto de apresentação
 st.write("---")
@@ -15,17 +15,17 @@ st.write("Olá! Me chamo Alysson, um engenheiro de dados entusiasmado e dedicado
 
 #cria uma função para carregar os dados fora do container
 #armazena os dados em cache para que não seja necessário recarregar numa segunda execução
-@st.cache_data
+#@st.cache_data
 def carrega_dados1():
     tabela_skill = pd.read_csv("SKILLS.csv")
     return tabela_skill
 
-@st.cache_data
+#@st.cache_data
 def carrega_dados2():
     tabela_curso = pd.read_excel("Cursos.xlsx")
     return tabela_curso
-st.write("---")
 
+st.write("---")
 #tabela de cursos
 st.write("CURSOS / CERTIFICADOS / GRADUAÇÕES")
 st.write("---")
@@ -41,35 +41,30 @@ st.write("---")
 with st.container():
     dados=carrega_dados1()
     st.bar_chart(dados, x="Nível", y="Habilidade",color="Nível")
-    st.write("LEGENDA:")
-    st.write("1-Iniciante")
-    st.write("2-Apto")
-    st.write("3-Intermediário")
-    st.write("4-Competente")
-    st.write("5-Experiente")
-    st.write("6-Avançado")
-    st.write("7-Diferenciado")
-    st.write("8-Mestre")
+    st.write("---")
+    # textos da legenda
+    legenda = 	"LEGENDA:", "1-Iniciante", "2-Apto", "3-Intermediário", "4-Competente", "5-Experiente", "6-Avançado", "7-Diferenciado", "8-Mestre"
+    # imprime legenda
+    for linha in legenda:
+        st.write(linha)
+
 st.write("---")
 #lista de soft Skills
 with st.container():
-    st.write("SOFT SKILLS")
-    st.write("---")
-    st.write("Flexibilidade: Adaptabilidade a novos ambientes e desafios.")
-
-    st.write("Resiliência Profissional: Habilidade de lidar com pressão e superar obstáculos.")
-
-    st.write("Aprendizado Contínuo: Compromisso com o desenvolvimento pessoal e profissional.")
-
-    st.write("Atuação junto de agentes multidisciplinares")
-
-    st.write("Resolução de Problemas: Identificar problemas e implementar soluções eficazes1.")
-
-    st.write("Ética de Trabalho: Demonstrar comprometimento, responsabilidade e dedicação ao trabalho")
+    st.write("SOFT SKILLS :handshake:")
+    # Usando st.markdown com HTML para criar um divisor colorido
+    st.markdown('<hr style="border:2px solid rainbow">', unsafe_allow_html=True)
+    # lista das soft skills
+    soft_skills = "Flexibilidade: Adaptabilidade a novos ambientes e desafios.";"Resiliência Profissional: Habilidade de lidar com pressão e superar obstáculos.";"Aprendizado Contínuo: Compromisso com o desenvolvimento pessoal e profissional.";"Atuação junto de agentes multidisciplinares";"Resolução de Problemas: Identificar problemas e implementar soluções eficazes1.";"Ética de Trabalho: Demonstrar comprometimento, responsabilidade e dedicação ao trabalho"
+    # divide em linhas
+    linhas = soft_skills.split(';')
+    # imprime as linhas de soft skills
+    for linha in linhas:
+        st.write(linha)
 
 st.write("---")
 with st.container():
-    st.write("LinkedIn pelo [LINK](https://www.linkedin.com/in/alyssonbarbara/)")
-
-    st.write("Whatsapp pelo [LINK](https://wa.me/qr/QBC2WTSAJTDBN1)")
+    st.write(":blue[LinkedIn] pelo [LINK](https://www.linkedin.com/in/alyssonbarbara/)")
+    st.write("---")
+    st.write(":green[Whatsapp] pelo [LINK](https://wa.me/qr/QBC2WTSAJTDBN1)")
 st.write("---")
